e4ace74bb8839cc6e97ad222a6d6e6749c90b0a8
soloman-err <soloman.err@gmail.com>
2023-05-02 01:00:00 +0600

    node express hosted on vercel

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 4000;
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hey Genius! you're observing Cocinero");
