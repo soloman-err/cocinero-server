863d41e142d43e4557cec907705105be2e7bc45b
soloman-err <soloman.err@gmail.com>
2023-05-02 11:28:00 +0600

    added featured dishes data

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 
+// server data:
+const featured = require('./data/featured-dishes.json');
 
+// cors:
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hey Genius! you're observing Cocinero");
+})
+
+app.get('/featured', (req, res) => {
+    res.send(featured);
 })
 
 app.listen(port, () => {
